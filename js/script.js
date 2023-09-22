2a7ed08c325b2965f145db188b2bb2f85ee984d0
Ana Pires <ana.pires@palmas.to.gov.br>
2023-09-22 10:32:22 -0300

    feat: :sparkles: adicionando icons e personalizando botao de submit.

--- js/script.js
+++ js/script.js
@@ -1,21 +1,17 @@
-function login() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
 
-    try {
-        if (username == '' || password ==''){
-            throw "Por favor, insira seu nome de usuário e senha.";
-        } else {
-            if (username == 'admin' && password == 'admin123') {
-            window.location.href = "https://ww2.uft.edu.br//";
-            } else {
-                alert("Login falhou. Verifique seu nome de usuário e senha.");
+        function login() {
+            var username = document.getElementById("username").value;
+            var password = document.getElementById("password").value;
+
+            let login_adm ="admin"
+            let password_adm = "admin"
+               
+            if( (username == login_adm) && (password == password_adm)){
+                    alert("Login bem-sucedido para " + username);
+                    
             }
-        }
-    }
-    catch(err) {
-        alert(err);
-        return;
-    }
-
-}
+            else{
+                    alert(" username e/ou password incorreto " );
+                    
+                    }
+                }
